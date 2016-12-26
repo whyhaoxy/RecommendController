f21738cda63b31b2ccd3d7ce0273729174fba685
LambooZ <627263110@qq.com>
2016-12-27 01:59:00 +0800

    1

---
+++ RecommendController.java
@@ -0,0 +1,10 @@
+package cn.edu.bjtu.weibo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/r")
+public class RecommendController {
+	
+}
